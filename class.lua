86e21b40c23d131906c52050e4db881feec0ecf6
YenYuan <64084912+lilith-avatar-yenyuan@users.noreply.github.com>
2020-05-24 01:16:02 +0800

    增加class类，修改luajson的内容

---
+++ class.lua
@@ -0,0 +1,23 @@
+---创建对象
+---@param _name string
+---@return class
+class = function (_name)
+    ---@type class
+    local re
+    return re
+end
+
+
+---面向对象的类
+---@class class
+local class = {}
+
+---@type any
+class.static = nil
+
+---打印对象对应的结果
+function class:__toString()
+end
+
+
+return class
