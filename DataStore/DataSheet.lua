259296145ed5f41411292c702738e0d6427a5681
lilith-avatar-yenyuan <64084912+lilith-avatar-yenyuan@users.noreply.github.com>
2020-07-02 15:24:59 +0800

    添加注释

--- DataStore/DataSheet.lua
+++ DataStore/DataSheet.lua
@@ -1,22 +1,26 @@
 ---@class DataSheet
 local DataSheet = {}
 
+---获取数据表里某个键对应的值，当获取成功或失败时触发回调函数。
 ---@param key string
 ---@param callBackFunc function
 function DataSheet:GetValue(key, callBackFunc)
 end
 
+---将数据表里某个键对应的值增加某个数值，当增加数值成功或失败时触发回调函数。
 ---@param key string
 ---@param addedValue number
 ---@param callBackFunc function
 function DataSheet:IncValue(key, addedValue, callBackFunc)
 end
 
+---移除数据表里的某个键值对。
 ---@param key string
 ---@param callBackFunc function
 function DataSheet:Remove(key, callBackFunc)
 end
 
+---将数据表里某个键设为相应的值，当设置成功或失败时触发回调函数。
 ---@param key string
 ---@param value any
 ---@param callBackFunc function
--- DataStore/DataStoreObject.lua
+++ DataStore/DataStoreObject.lua
@@ -1,7 +1,10 @@
 ---@class DataStoreObject
 local DataStoreObject={}
+
+---获取表对象。
 ---@param sheetName string
 ---@return DataSheet
 function DataStoreObject:GetSheet(sheetName)
 end
+
 return DataStoreObject
