cd47ef3fc8bb7b207eb6c392f12864f5371176e0
lilith-avatar-yenyuan <64084912+lilith-avatar-yenyuan@users.noreply.github.com>
2020-07-02 18:27:46 +0800

    修改animatedMeshObject playAnimation api

--- Construct/AnimatedMeshObject.lua
+++ Construct/AnimatedMeshObject.lua
@@ -2,15 +2,25 @@
 local AnimatedMeshObject = {}
 
 AnimatedMeshObject.AngularVelocity = Vector3.Zero
+
 AnimatedMeshObject.Block = true
+
 AnimatedMeshObject.CollisionGroup = 0
+
 AnimatedMeshObject.Density = 0
+
 AnimatedMeshObject.FrictionRate = 0
+
 AnimatedMeshObject.GravityEnable = true
+
 AnimatedMeshObject.GravityScale = 0
+
 AnimatedMeshObject.IsStatic = true
+
 AnimatedMeshObject.LinearVelocity = Vector3.Zero
+
 AnimatedMeshObject.Restitution = 0
+
 AnimatedMeshObject.Rough = 0
 
 ---给某个动作在特定的百分比时间添加一个触发事件。
@@ -18,7 +28,7 @@ AnimatedMeshObject.Rough = 0
 ---@param percent number
 function  AnimatedMeshObject:AddAnimationEvent(animationName, percent) end
 
----给某个动作在特定的百分比时间添加一个触发事件。
+---播放动作。
 ---@param name string
 ---@param layer number
 ---@param weight number
@@ -26,7 +36,7 @@ function  AnimatedMeshObject:AddAnimationEvent(animationName, percent) end
 ---@param interrupt boolean
 ---@param loop boolean
 ---@param speedScale number
-function  PlayAnimation:AddAnimationEvent(name, layer, weight, transitionDuration, interrupt, loop, speedScale) end
+function  AnimatedMeshObject:PlayAnimation(name, layer, weight, transitionDuration, interrupt, loop, speedScale) end
 
 ---设置混合树。
 ---@param boneName string
