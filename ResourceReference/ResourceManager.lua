d32ae3b23907579dcb5cd1712f40e67d1b790b9c
lilith-avatar-yenyuan <64084912+lilith-avatar-yenyuan@users.noreply.github.com>
2020-07-06 21:27:02 +0800

    更改api为静态函数

--- ResourceReference/ResourceManager.lua
+++ ResourceReference/ResourceManager.lua
@@ -4,16 +4,16 @@ local ResourceManager = {}
 ---获取模型资源的引用。
 ---@param meshPath string
 ---@return MeshRef
-function  ResourceManager:GetMesh(meshPath) end
+function  ResourceManager.GetMesh(meshPath) end
 
 ---获取音频资源的引用。
 ---@param soundPath string
 ---@return SoundClipRef
-function  ResourceManager:GetSoundClip(soundPath) end
+function  ResourceManager.GetSoundClip(soundPath) end
 
 ---获取贴图资源的引用。
 ---@param texturePath string
 ---@return TextureRef
-function  ResourceManager:GetTexture(texturePath) end
+function  ResourceManager.GetTexture(texturePath) end
 
 return ResourceManager
